a676a05598a7427c56bceb387152993daf67a8bd
Jon Oblander <jonoblander@pingidentity.com>
2022-07-19 09:58:09 -0700

    save changes

--- src/types/as-init-options.ts
+++ src/types/as-init-options.ts
@@ -4,12 +4,6 @@ export interface AsInitOptions {
   /** PingFederate base path */
   BasePath: string;
 
-  /** oAuth client id */
-  ClientID: string;
-
-  /** */
-  RedirectURI: string;
-
   /** Optional - Level will be set to warning by default */
   LoggingLevel?: LogLevel;
 }
