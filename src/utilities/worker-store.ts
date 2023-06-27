dc9709cb2232123212a2d059cfcab2ce7cb22754
michaelsanchez-pingidentity <michaelsanchez@pingidentity.com>
2023-06-27 16:47:10 -0700

    Continued updates. #185252416

--- src/utilities/worker-store.ts
+++ src/utilities/worker-store.ts
@@ -1,57 +1,104 @@
 /**
  * Wrapper subclass representing a Web Worker for in-memory storage in a separate thread.
- * This has to be a wrapper class because a Web Worker cannot access the main thread.
- * @see worker-thread.ts
+ * This has to be a wrapper class because a Web Worker cannot access the main thread and vice versa.
+ * @see worker-thread.js
  * {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Functions_and_classes_available_to_workers}
  */
 
 import { TokenResponse } from '../types/token-response';
 import { ClientStorage } from './client-storage';
+import OAuth from './oauth';
+
+// Local interface unique to this subclass
+export interface WorkerMessage {
+  // Name of method posting message to the Web Worker
+  method: string;
+  // Optional payload. The data sent to the Web Worker to be stored.
+  payload?: string | object;
+}
 
 export class WorkerClientStorage extends ClientStorage {
-  readonly TOKEN_KEY: string;
-  readonly REFRESH_TOKEN_KEY: string;
-  readonly CODE_VERIFIER_KEY: string;
-  private workerThread;
+  readonly TOKEN_KEY = 'oidc-client:response';
+  readonly REFRESH_TOKEN_KEY = 'oidc-client:refresh_token';
+  readonly CODE_VERIFIER_KEY = 'oidc-client:code_verifier';
+  private readonly workerThread;
+  private msg: WorkerMessage;
 
   constructor() {
     super();
-    this.workerThread = new Worker('./worker-thread.ts');
+    this.workerThread = new Worker('./worker-thread.js');
   }
 
   override storeToken(token: TokenResponse): void {
-    const msg = { method: 'storeToken', payload: token };
-    this.workerThread.postMessage(msg);
+    const refreshToken = token.refresh_token;
+
+    if (refreshToken) {
+      // eslint-disable-next-line no-param-reassign
+      delete token.refresh_token;
+      const refreshKey = this.REFRESH_TOKEN_KEY;
+      this.msg = { method: 'storeToken', payload: { refreshKey: OAuth.btoa(refreshToken) } };
+      this.workerThread.postMessage(this.msg);
+    } else {
+      // Remove old refresh token just in case, would be weird to hit this...
+      this.msg = { method: 'removeToken', payload: this.REFRESH_TOKEN_KEY };
+      this.removeToken();
+    }
+
+    const str = JSON.stringify(token);
+    const tokenKey = this.TOKEN_KEY;
+    this.msg = { method: 'storeToken', payload: { tokenKey: OAuth.btoa(str) } };
+    this.workerThread.postMessage(this.msg);
   }
 
   override getToken(): TokenResponse {
-    const msg = { method: 'getToken' };
-    this.workerThread.postMessage(msg);
-    // TODO how do we handle this for all methods???
-    this.workerThread.onmessage = (response) => {
-      return { response };
-    };
+    this.msg = { method: 'getToken', payload: this.TOKEN_KEY };
+    const encodedStr = this.workerThread.postMessage(this.msg);
+
+    if (encodedStr) {
+      const decodedStr = OAuth.atob(encodedStr);
+      const token = JSON.parse(decodedStr);
+
+      return token;
+    }
+
+    return null;
   }
 
   override getRefreshToken(): string {
-    const msg = { method: 'getRefreshToken' };
-    this.workerThread.postMessage(msg);
-    // TODO need return value from onmessage event handler.
+    this.msg = { method: 'getRefreshToken', payload: this.REFRESH_TOKEN_KEY };
+    const refreshToken = this.workerThread.postMessage(this.msg);
+
+    // Self destruct on retrieval, only needed once when refreshToken is called
+    this.msg = { method: 'removeToken', payload: this.REFRESH_TOKEN_KEY };
+    this.removeToken();
+    return refreshToken ? OAuth.atob(refreshToken) : null;
   }
 
   override removeToken(): void {
-    const msg = { method: 'removeToken' };
-    this.workerThread.postMessage(msg);
+    if (this.msg?.payload) {
+      // Remove specific item
+      this.workerThread.postMessage(this.msg);
+    } else {
+      // Remove all tokens and data
+      this.msg = { method: 'removeToken' };
+      this.workerThread.postMessage(this.msg);
+    }
   }
 
   override storeCodeVerifier(codeVerifier: string): void {
-    const msg = { method: 'storeCodeVerifier', payload: codeVerifier };
-    this.workerThread.postMessage(msg);
+    const verifierKey = this.CODE_VERIFIER_KEY;
+    this.msg = { method: 'storeCodeVerifier', payload: { verifierKey: OAuth.btoa(codeVerifier) } };
+    this.workerThread.postMessage(this.msg);
   }
 
   override getCodeVerifier(): string {
-    const msg = { method: 'getCodeVerifier' };
-    this.workerThread.postMessage(msg);
-    // TODO need return value from onmessage event handler.
+    this.msg = { method: 'getCodeVerifier', payload: this.CODE_VERIFIER_KEY };
+    const encodedStr = this.workerThread.postMessage(this.msg);
+
+    // Self destruct on retrieval, only needed once to get the token from the authorization server
+    this.msg = { method: 'removeToken', payload: this.CODE_VERIFIER_KEY };
+    this.removeToken();
+
+    return encodedStr ? OAuth.atob(encodedStr) : null;
   }
 }
--- src/utilities/worker-thread.js
+++ src/utilities/worker-thread.js
@@ -1,41 +1,77 @@
 /**
- * Class representing a Web Worker for in-memory storage in a separate thread.
+ * Web Worker for in-memory storage that runs in a separate thread from the main app.
  * {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers}
  * {@link https://developer.mozilla.org/en-US/docs/Web/API/Worker}
  */
 
-// TODO do we need to wrap these in a closure?
-let tokenCache = {};
-let refreshTokenCache = {};
-let codeVerifierCache = '';
+// Object to store tokens and data in memory.
+const oauthCache = []; // I.e. [{},{},{}]. The payload portions of the inbound message for store methods.
+let responseMsg = {}; // A specific object from oathCache.
+
+// sample inbound objects - delete this comment
+// { method: 'getToken', payload: this.TOKEN_KEY };
+// { method: 'storeToken', payload: { this.TOKEN_KEY, OAuth.btoa(token) } }
 
 // eslint-disable-next-line no-undef
-onmessage = (msg) => {
-  switch (msg.method) {
+onmessage = async (inboundMsg) => {
+  switch (inboundMsg.method) {
     case 'storeToken':
-      tokenCache = msg.payload;
-      refreshTokenCache = msg.payload; // TODO update for refresh token
+      if (oauthCache.length === 0) {
+        oauthCache.push(inboundMsg.payload);
+      } else {
+        // TODO Array.map() or Array.splice() to add or update object.
+      }
       break;
     case 'getToken':
+      responseMsg = oauthCache.find((element) => element.payload === inboundMsg.payload);
+      // TODO need to parse out the token before posting message.
       // eslint-disable-next-line no-undef
-      postmessage(tokenCache);
+      postmessage(responseMsg);
       break;
     case 'getRefreshToken':
+      responseMsg = oauthCache.find((element) => element.payload === inboundMsg.payload);
+      // TODO need to parse out the token before posting message.
       // eslint-disable-next-line no-undef
-      postmessage(refreshTokenCache);
+      postmessage(responseMsg);
       break;
     case 'removeToken':
-      tokenCache = {};
-      refreshTokenCache = {};
+      if (inboundMsg?.payload) {
+        // delete specific object by inboundMsg.payload
+      } else {
+        // Delete all the thingz
+        oauthCache.length = 0;
+      }
       break;
     case 'storeCodeVerifier':
-      codeVerifierCache = msg.payload; // TODO update for code verifier
+      if (oauthCache.length === 0) {
+        oauthCache.push(inboundMsg.payload);
+      } else {
+        // TODO Array.map( OR Array.splice() to add or update object.
+      }
       break;
     case 'getCodeVerifier':
+      responseMsg = oauthCache.find((element) => element.payload === inboundMsg.payload);
+      // TODO need to parse out the token before posting message.
       // eslint-disable-next-line no-undef
-      postmessage(codeVerifierCache);
+      postmessage(responseMsg);
       break;
     default:
       throw new Error();
   }
 };
+
+// TODO Could this replace the switch/case statement?
+// function updateOauthCache(inMsg) {
+//   const newOauthCache = oauthCache.map((currentObj) => {
+//     if (currentObj.key === inMsg.key) {
+//       // look at inMsg.method and update / return object accordingly.
+//       return 'update';
+//     }
+//     return currentObj;
+//   });
+//   oauthCache = newOauthCache;
+// }
+
+// sample inbound objects - delete this comment
+// { method: 'removeToken', payload: this.TOKEN_KEY };
+// { method: 'storeCodeVerifier', payload: { this.CODE_VERIFIER_KEY, OAuth.btoa(codeVerifier) } }
